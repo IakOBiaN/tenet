93735d7c2da072d1470bde922295d5023ab31545
iakobian <iakobian@gmail.com>
2023-04-20 11:13:04 +0600

    Time calculation for mono test

--- test_mono.py
+++ test_mono.py
@@ -1,4 +1,7 @@
 import MainScripts as ms
+import timeit
+
+start_time = timeit.default_timer()
 
 #mono with interactions
 method = "trg"
@@ -7,7 +10,8 @@ lattice = "square"
 #model params
 T = 120.0
 chi_number = 10
-for mu in ms.np.arange(-10.00, 10.01, 0.5):
-	m_par = [mu, 2.0, 0, 0, 0, 0]
+for mu in ms.np.arange(-10.00, 20.01, 0.5):
+	m_par = [mu, 4.0, 0, 0, 0, 0]
 	result = ms.full(method, model, lattice, chi_number, T, m_par)
-	print(mu, result[0], result[1] , result[2] , result[3])
+	calc_time = timeit.default_timer() - start_time
+	print(mu, result[0], result[1] , result[2] , result[3], calc_time)
