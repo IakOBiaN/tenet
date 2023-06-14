4bd80e89683e061bfab1f749aeb95ec15b6fddf2
iakobian <iakobian@gmail.com>
2023-06-14 15:38:34 +0600

    Correction for heat capacity in exact solution

--- Additional/exact_solution_for_ising_model.py
+++ Additional/exact_solution_for_ising_model.py
@@ -24,7 +24,7 @@ def exact(T = 1.0, J = 1.0, lattice = "square"):
 
 def heat_capasity(T = 1.0, J = 1.0, lattice = "square"):
 	result = derivative(lambda x: exact(x, J, lattice)[0], T, n=2, dx=1e-5)
-	return result
+	return (result, )
 
 T1 = 2.0/log(1+sqrt(2))
 T2 = 4.0/log(3)
