0bb8ea93d8979f813016ecb7cb716d8530b99b9b
Sergey <iakobian@gmail.com>
2024-12-12 14:29:29 +0600

    Correction for BTRG. Graphs were updated.

--- Scripts/MainScripts.py
+++ Scripts/MainScripts.py
@@ -79,7 +79,7 @@ def simulate(calc, T = 1.0, m_par = [0.0] * 10):
 			assert False, "Error! There is no such method."
 
 		if calc.method != "htn":
-			if abs(old_scale - scale / calc.scale) < calc.methodTolerance and i > minimum_iterations:
+			if (abs(old_scale - scale / calc.scale) < calc.methodTolerance and i > minimum_iterations) or (tensors[0].size < 2):
 				break
 			else:
 				old_scale = scale
